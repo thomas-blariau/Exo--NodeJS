f07054973217cf4dea7f21718f2663fa7b2be369
Thomas Blariau <53513961+thomas-blariau@users.noreply.github.com>
2019-09-11 16:29:43 +0200

    Update search.controller.js

--- ressources/movies/search.controller.js
+++ ressources/movies/search.controller.js
@@ -6,7 +6,6 @@ export const search = async (req, res) => {
         const movieSearch = await Movie.find(req.query);
         // if (!movie) return res.status(400).end();
         res.status(200).json({ movie:movieSearch });
-        // console.log(movie)
 
     } catch (err) {
         console.log(err);
